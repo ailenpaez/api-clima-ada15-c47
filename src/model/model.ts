9f82c68c11210003109c992ac887d64b89bbf3d7
ailenpaez <a.jorgelinapaez@gmail.com>
2023-12-09 23:42:52 -0300

    Cambio de nombre a la carpeta 'interfaces' por 'utils'

--- src/model/model.ts
+++ src/model/model.ts
@@ -1,6 +1,6 @@
 import jsonfile from "jsonfile";
 import { pathFile } from "../database";
-import *  as interfaces from "../interfaces/interfaces"
+import *  as interfaces from "../utils/interfaces"
 
 // FUNCION PARA DEVOLVER TODO LO DE DATABASE
 const getDataWeather = (): interfaces.Weather => jsonfile.readFileSync(pathFile);
